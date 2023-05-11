25b4374e123fb7660ef6b98650ef076b6f199d61
Ericson-Manalo <ericmana98@gmail.com>
2023-05-11 15:41:16 +0200

    created variable surname + fav colour

--- js/myscript.js
+++ js/myscript.js
@@ -1,1 +1,9 @@
 const userName = prompt ('Insert your name');
+
+const userSurname = prompt ('Insert you surname');
+
+const favouriteColor = prompt ('Insert your favourite color');
+
+const pswGenerated = userName + userSurname + favouriteColor;
+
+document.getElementById('psw-generated').innerHTML = pswGenerated;
