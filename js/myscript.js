40471f59cb73f30f58274c84d3f54bec837cb628
Ericson-Manalo <ericmana98@gmail.com>
2023-05-11 16:19:36 +0200

    added some style

--- js/myscript.js
+++ js/myscript.js
@@ -1,9 +1,14 @@
+
+// created a variable const for user's Name
 const userName = prompt ('Insert your name');
 
+// created a variable const for user's Surname
 const userSurname = prompt ('Insert you surname');
 
+// created a variable const for user's Favourite colour
 const favouriteColor = prompt ('Insert your favourite color');
 
+// created a variable to add all the variable listed above 
 const pswGenerated = userName + userSurname + favouriteColor + 21;
 
 document.getElementById('psw-generated').innerHTML = pswGenerated;
